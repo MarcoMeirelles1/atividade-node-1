cf06331f307fe3db26b8ba9c32b75aabcaefcbe2
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-02 16:38:14 -0300

    up

--- AulaNode/index.js
+++ AulaNode/index.js
@@ -8,22 +8,37 @@ app.use(express.json());
 
 app.get('/visualizar', (req, res) => {
     res.send(list);
-}); // Responde com a lista de usuários
+}) // Responde com a lista de usuários
 
 app.post('/cadastrar', (req, res) => {
     const { nome, idade } = req.body;
-    list.push({ nome, idade });
+    const id = list.length;
+    list.push({ id, nome, idade });
     res.send(`Usuário cadastrado: Nome: ${nome}, Idade: ${idade}`);
+}) //cadastra um usuario
+
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+    try{
+        list[id - 1] = {id, nome, idade};
+        res.send(`Usuario atualizado!\n\nID: ${id}\nNovo nome: ${nome} \nNova idade ${idade}`)
+    } catch(err) {
+        res.send("Usuario não atualizado");
+    }
 });
 
-
 app.delete('/deletar/:id', (req, res) => {
-    const { id } = req.params; // Obtém o nome do usuário a ser deletado
-    const index = parseInt(id,10) // Encontra o índice do usuário
-    list.splice(index,1)
-    res.send(`usuario deletado ${JSON.stringify(list)}`)
-}); // Deleta um usuário pelo nome
+    const { id } = req.params; 
+    if (list[id - 1]) {
+        list.slice(id - 1, 1);
+        res.send('usuario deletado')
+    } else {
+        res.status(408).send('usuario não encontrado');
+    }
+
+}) 
 
 app.listen(port, () => {
-    console.log(`Exemplo de Maricon na porta ${port}`);
-}); // Inicia o servidor
+    console.log(`entrada ${port}`);
+}) // Inicia o servido
